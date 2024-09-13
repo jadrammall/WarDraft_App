64c333e34a6e20bda5a7edffa4c4fc30e254b983
jadrammall <jrammal12@gmail.com>
2024-09-13 11:22:36 +0300

    Initial Commit

---
+++ WarDraft_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WarDraft_App
+//
+//  Created by Jad Rammal on 13/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
