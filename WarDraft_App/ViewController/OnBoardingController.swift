45aa1031d83f1a68438b7f6f59638c50bee1db86
jadrammall <jrammal12@gmail.com>
2024-09-13 15:22:46 +0300

    add RegisterView

--- WarDraft_App/ViewController/OnBoardingController.swift
+++ WarDraft_App/ViewController/OnBoardingController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class OnBoardingController: UIViewController {
 
     @IBOutlet weak var registerButton: UIButton!
     @IBOutlet weak var loginButton: UIButton!
